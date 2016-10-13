e1ce12c4c26d90c07d6a7a4fe1cbf6e779a7294c
Wilmer Ramones <wramones@ubv.edu.ve>
2016-10-13 11:17:57 -0400

    añadido la actividad docente a la plantilla del formulario de solicitud del PIDA

--- src/AppBundle/Form/PidaType.php
+++ src/AppBundle/Form/PidaType.php
@@ -9,46 +9,50 @@
 
 namespace AppBundle\Form;
 
-use Symfony\Component\Validator\Constraints\File;
-use Symfony\Component\Validator\Constraints\NotBlank;
-
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Doctrine\ORM\EntityRepository;
 
 class PidaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            
-
-            ->add('plan_historico_nacional_estrategico', EntityType::class, array(
+                
+                ->add('plan_historico_nacional_estrategico', EntityType::class, array(
                 'label'         => false,
                 'attr' => array(
                     'class' =>  'select2'
                 ),
                 
                 'placeholder' => 'Seleccione Plan Nacional que se relaciona con su Actividad PIDA',
-                'required' => false,
+                'required' => true,
                 'class' => 'AppBundle:PlanHistoricoNacionalEstrategico',
                 'choice_label' => 'getNombre',
                 'group_by'      => 'getIdPlanHistoricoNacional'
 
-            ))
+                ))
 
-           
+                ->add('actividad_docente', EntityType::class, array(
+                'label'         => false,
+                'attr' => array(
+                    'class' =>  'select2'
+                ),
+                
+                'placeholder' => '¿Bajo cúal actividad docente está enmarcado dicho objetivo?',
+                'required' => true,
+                'class' => 'AppBundle:ActividadDocente',
+                'choice_label' => 'getNombre'                
 
-            
-		 
-
+                ))
+                
+                
+                ->add('send', SubmitType::class, array(
+                      'label' => 'Enviar Actividad PIDA',
+                      'attr'  => array('class' => 'btn btn-success btn-block')
+                ))
 
         ;
 
