d593aa110a9fdfbd0d6dbe88a3618d447dbd56ba
Wilmer <wilmer.ramones@gmail.com>
2017-03-09 08:42:54 -0400

    AscensoType mejorado en errores y quitar lo que no se utiliza

--- src/AppBundle/Form/AscensoType.php
+++ src/AppBundle/Form/AscensoType.php
@@ -15,14 +15,12 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Doctrine\ORM\EntityRepository;
 
 class AscensoType extends AbstractType
 {
